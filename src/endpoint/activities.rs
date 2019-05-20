564d3ecb159dc0fdfcad7d0d86d0045b4e0a0bce
Matthew Palmer <matt@silvercreekit.com>
2019-05-19 22:03:17 -0400

    activities endpoint

--- src/endpoint/activities.rs
+++ src/endpoint/activities.rs
@@ -2,41 +2,55 @@ use crate::client::Client;
 use reqwest::Error;
 use serde::Deserialize;
 
-pub fn get_listing(client: &Client) -> Result<Activity, Error> {
+pub fn get_listing(client: &Client) -> Result<Vec<Activity>, Error> {
     Ok(client.request("/activities")?.json()?)
 }
 
+pub fn get_listing_by_id(client: &Client, id: u32) -> Result<Activity, Error> {
+    Ok(client.request(&format!("/activities/{}", id))?.json()?)
+}
+
 /// Activities ordered by inverse creation date.
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 pub struct Activity {
     pub id: u32,
     pub title: String,
     pub content: String,
-    pub date: String, // date
+    pub date: String,
     pub links: Vec<String>,
     pub observing_site: String,
     pub telescope: String,
     pub instrument: String,
     pub target_name: String,
-    pub profile: String, // enum
-    // pub collaboration: Some(String), // enum
-    // pub organisation: Some(String), // enum
-    pub creation_date: String, // date
+    pub profile: String,
+    pub collaboration: Option<String>,
+    pub organisation: Option<String>,
+    pub creation_date: String,
     pub coordinates: Coordinates,
     pub programme: Programme,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
+pub enum CoordinatesSystem {
+    ICRS,
+    FK5,
+    FK4,
+    FK4NoETerms,
+    Galactic,
+    AltAz,
+}
+
+#[derive(Deserialize)]
 pub struct Coordinates {
-    pub system: String, // enum
+    pub system: CoordinatesSystem,
     pub right_ascension: f64,
-    pub right_ascension_units: String, // enum
+    pub right_ascension_units: String,
     pub declination: f64,
-    pub declination_units: String, // enum
+    pub declination_units: String,
     pub epoch: u128,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 pub struct Programme {
     pub id: u32,
     pub programme_id: String,
